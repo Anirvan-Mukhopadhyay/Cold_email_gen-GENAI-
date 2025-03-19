ba1be774ec9e44597e76cbbdca24296cecf63fae
Anirvan Mukhopadhyay <mukhopadhyayanirvan@gmail.com>
2025-03-19 18:44:05 +0530

    Update main.py

--- resources/main.py
+++ resources/main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 from langchain_community.document_loaders import WebBaseLoader
 from chains import Chain
-from portfolio import Portfolio
+from portfolio_nodule import Portfolio
 from utils import clean_text
 
 # Set environment variables
